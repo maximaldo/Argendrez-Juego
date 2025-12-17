765ed95606b75d00f740d16ab0c01bca4583dcc3
maximaldo <maxigram1@hotmail.com>
2025-12-17 02:56:30 -0300

    JUEGO TERMINADO

--- core/src/main/java/Principal/juego/Principal.java
+++ core/src/main/java/Principal/juego/Principal.java
@@ -2,39 +2,17 @@ package Principal.juego;
 
 import com.badlogic.gdx.Game;
 import Principal.juego.interfaz.MenuPantalla;
-import Principal.juego.red.ClienteAjedrez;
 
 public class Principal extends Game {
 
-    private ClienteAjedrez clienteRed;
+
 
     @Override
     public void create() {
         setScreen(new MenuPantalla(this));
     }
 
-    public void crearCliente(ClienteAjedrez.ReceptorMensajes receptor) {
-        try {
-            clienteRed = new ClienteAjedrez(receptor);
-            clienteRed.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public ClienteAjedrez getClienteRed() {
-        return clienteRed;
-    }
-
     public void dispose() {
-        if (clienteRed != null) {
-            clienteRed.cerrar(); // Envía el paquete "Disconnect" y cierra el socket
-            try {
-                clienteRed.join(1000); // Esperar un momento a que el hilo muera limpiamente
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
         super.dispose();
     }
 }
